b7e9ab3c75c4e9351f27a0bd501fc80ef301554d
Gaston Nill <Gastok1979@gmail.com>
2023-03-02 18:40:12 -0300

    Primer escritura en Proyecto Currriculum

---
+++ JavaScript/Java.js
@@ -0,0 +1,7 @@
+document.getElementById('boton Correo').onclick = function() {
+    document.getElementById("Email").innerText = "Gastok1979@gmail.com"
+}
+
+document.getElementById('boton telefono').onclick = function() {
+    document.getElementById("telefono").innerText = "(291) 5711936"
+}
